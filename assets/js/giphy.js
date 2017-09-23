ee5cee8717ad7731f237fb78244a235b95788c4e
Jeffrey Lowy <jeffreylowy@gmail.com>
2017-09-23 01:19:47 -0700

    Ready for submission

--- assets/js/giphy.js
+++ assets/js/giphy.js
@@ -2,7 +2,7 @@
 var giphy = {};
 
 // Store the default set of movies
-giphy.movies = ["Wayne's World", "The Dark Knight", "Big Trouble in Little China", "The Empire Strikes Back", "The Matrix", "Guardians of the Galaxy", "Harlem Nights", "The Godfather", "Pulp Fiction", "Pee-Wee's Big Adventure", "Fight Club", "12 Monkeys", "Back to the Future", "Ferris Bueller", "Army of Darkness", "Nacho Libre", "Die Hard", "The Fifth Element"];
+giphy.movies = ["Akira", "Blade Runner", "The Matrix", "Sin City", "Ghost in the Shell", "Star Trek: Beyond", "The Fifth Element", "Mad Max: Fury Road", "Rogue One", "Ex Machina", "2001: A Spacee Odyssey", "Interstellar", "Tron: Legacy"];
 
 // An array of classes to add to the buttons
 giphy.buttonClasses = ["bttn-minimal", "bttn-xs"];
@@ -12,31 +12,58 @@ giphy.initializePage = function (movies) {
 
 	// Store a reference of the jumbotron in a variable with the same name
 	var jumbotron = $("#jumbotron");
+	var movies = $("#movies");
 
 	// Create the h1 for the title and add a line break
 	var h1 = document.createElement("h1"),
 		br = document.createElement("br");
 	h1.innerText = "Choose a movie!";
-	jumbotron.append(h1);
-	jumbotron.append(br);
+	jumbotron.prepend(h1);
+	jumbotron.prepend(br);
 
 	// Loop through the default movies array using jQuery forEach method
-	giphy.movies.forEach(function (movie) {
-		var a = document.createElement("a");
-		a.setAttribute("data-movie", movie);
-		a.innerText = movie;
-		a.classList.add(...giphy.buttonClasses);
-		jumbotron.append(a);
+	giphy.movies.forEach(giphy.createMovieItem);
+
+	// Add the input area using es6 template strings so I can separate the code to multiple lines and not worry about whitespace.
+	jumbotron.append(`
+		<form id="movie-form">
+			<div class="input-group">
+				<input id="movie-input" type="text" class="form-control" placeholder="Enter a movie title...">
+				<span id="submit-add" class="input-group-addon bttn-success bttn-material-flat"><button type="submit" class="glyphicon glyphicon-plus"></button></span>
+			</div>
+		</form>
+	`);
+
+	$("#submit-add").on("click", function (event) {
+		event.preventDefault();
+		var movieInputValue = $("#movie-input");
+		giphy.createMovieItem(movieInputValue.val());
+		movieInputValue.val("");
+	});
+	$("#movie-form").on("submit", function (event) {
+		event.preventDefault();
+		var movieInputValue = $("#movie-input");
+		giphy.createMovieItem(movieInputValue.val());
+		movieInputValue.val("");
+	});
+};
+
+// Method to add a movie to a list. Used in the initializePage method and the click handler for $("#submit-add")
+giphy.createMovieItem = function (movie) {
+	// Create a new element, set the data attribute, add text and css classes
+	var a = document.createElement("a");
+	a.setAttribute("data-movie", movie);
+	a.innerText = movie;
+	a.classList.add(...giphy.buttonClasses);
+
+	// The click handler for all of the buttons in the div.jumbotron that calls the getMovie which sends the data-movie's value as an argument
+	a.addEventListener("click", function () {
+		giphy.getMovie(movie);
 	});
 
-	// Add the input area using es6 template strings, because why not...
-	jumbotron.append(`
-		<div class="input-group">
-			<input type="text" class="form-control" placeholder="Add a movie.." aria-describedby="basic-addon2">
-			<span class="input-group-addon bttn-success bttn-material-flat" id="basic-addon2">ADD MOVIE</span>
-  		</div>
-	`);
-};
+	// Append the list to div#movies
+	movies.append(a);
+}
 
 // Wrap the $.ajax in the getMovie method
 giphy.getMovie = function (movie) {
@@ -46,9 +73,9 @@ giphy.getMovie = function (movie) {
 			data: {
 				api_key: "6039e9a9db5140b8b08b727c3e8bd933",
 				q: movie,
-				rating: "PG-13",
+				rating: "pg-13",
 				fmt: "json",
-				limit: 10
+				limit: 9
 			}
 		})
 		.done(function (response) {
@@ -59,8 +86,8 @@ giphy.getMovie = function (movie) {
 
 giphy.updateGiphyArea = function (movieResponse) {
 	// Empty the div containing the img elements
-	$(".giphyArea").empty();
-	
+	$(".giphyArea").empty()
+
 	// The data object of the response is an array containing all of the giphy data for the gif
 	var responseData = movieResponse.data;
 	console.log(responseData);
@@ -74,22 +101,35 @@ giphy.updateGiphyArea = function (movieResponse) {
 		// Create an img element, store the .gif's still frame, add a click handler and append to the new div
 		var img = $("<img>")
 			.attr("src", gif.images.fixed_height_still.url)
+			.attr("data-state", "still")
 			.on("click", function (event) {
-				$(this).attr("src", gif.images.fixed_height.url);
+				var state = $(this).attr("data-state");
+				console.log("IMG STATE", state);
+
+				if (state === 'still') {
+					$(this).attr("src", gif.images.fixed_height.url).attr("data-state", "playing");
+				} else if (state === 'playing') {
+					$(this).attr("src", gif.images.fixed_height_still.url).attr("data-state", "still");
+				} else {
+					console.warn("OH NO! The .gif's click handler is broken...")
+				}
+
 			});
+
+		var p = $("<p>").text(`Rating: ${gif.rating}`);
 
 		// Append the new div with the image to div.giphyArea
 		divColXs12.append(img);
-		$(".giphyArea").append(divColXs12);
+		divColXs12.append(p);
+		$(".giphyArea").delay(100).queue(function (next) {
+			$(this).append(divColXs12);
+			next();
+		});
+
+
+
 	});
 }
 
 // Run the initializePage method to load the page content
 giphy.initializePage(giphy.movies);
-
-// The click handler for all of the buttons in the div.jumbotron that calls the getMovie which sends the data-movie's value as an argument
-$(".bttn-xs").on("click", function (event) {
-	event.preventDefault();
-	var movie = $(this).data().movie;
-	giphy.getMovie(movie);
-});
--- readme.md
+++ readme.md
@@ -1,4 +1,30 @@
+# Summary
+I used some jQuery, mostly for the ajax calls, and creating/appending elements to the page.
+
+I also used some es6 features within jQuery callbacks. 
+
+[View on Github Pages](https://jeffreylowy.github.io/giphy_api/)
+
+```javascript
+	// Add the input area using es6 template strings so I can separate the code to multiple lines and not worry about whitespace.
+	jumbotron.append(`
+	<form id="movie-form">
+		<div class="input-group"><input id="movie-input" type="text" class="form-control" placeholder="Enter a movie title...">
+			<span id="submit-add" class="input-group-addon bttn-success bttn-material-flat">
+				<button type="submit" class="glyphicon glyphicon-plus"></button>
+			</span>
+		</div>
+	</form>`);
+
+	//...
+
+	var p = $("<p>").text(`Rating: ${gif.rating}`);
+	divColXs12.append(p);
+```
+
 ## Resources 
+
+[$.queue()](https://stackoverflow.com/questions/11085567/jquery-delay-to-work-with-append)
 
 [MDN: Spread syntax](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_operator)
 
